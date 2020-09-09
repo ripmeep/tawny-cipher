0ff7c71a38d486b751c2344494539afb431aea45
meep <36815692+ripmeep@users.noreply.github.com>
2020-09-09 03:39:17 +0000

    Add files via upload

---
+++ examples/decrypt.c
@@ -0,0 +1,111 @@
+#include <stdio.h>
+#include <string.h>
+#include <openssl/rand.h>
+
+#include "tawny.h"
+
+int main(int argc, char ** argv) {
+	if (argc < 3) {
+		fprintf(stderr, "\nUsage: [CIPHERTEXT FILE] [KEY FILE]\n\n");
+		return -1;
+	}
+
+	char * ciphertext_filename = argv[1];
+	char * key_filename = argv[2];
+
+	FILE * ciphertext_file = fopen(ciphertext_filename, "rb");
+	FILE * key_file = fopen(key_filename, "rb");
+
+	if (ciphertext_file == NULL || key_file == NULL) {
+		fprintf(stderr, "Failed to open file \"%s\" [fopen()]\n", (ciphertext_file == NULL)?argv[1]:argv[2]);
+		perror("Error");
+		return -1;
+	}
+
+	long ciphertext_len, key_len = 0;
+
+	fseek(ciphertext_file, 0, SEEK_END); 	/* GO TO END OF CIPHERTEXT FILE */
+	fseek(key_file, 0, SEEK_END); 		/* GET FIRST 32 BYTES OF KEY */
+
+	ciphertext_len = ftell(ciphertext_file);	/* STORE LENGTH OF CIPHERTEXT FILE - LENGTH OF IV AT END */
+	key_len = ftell(key_file);			/* STORE LENGTH OF ENCRYPTION KEY - 2 FOR LINE ENDING */
+
+	rewind(ciphertext_file);	/* RESET POSITION OF FILES TO THE START */
+	rewind(key_file);
+
+	if (key_len != TAWNY_BLOCK_SIZE_BYTES) { 	/* CHECK LENGTH OF KEY */
+		fprintf(stderr, "Key length incorrect (%ld)\nKey size must be 256 bits (32 bytes)\n", key_len);
+		return -1;
+	}
+
+	ciphertext_len = ciphertext_len - TAWNY_BLOCK_SIZE_BYTES;
+
+	char ciphertext[ciphertext_len];
+	char key[TAWNY_BLOCK_SIZE_BYTES];
+	char iv[TAWNY_BLOCK_SIZE_BYTES];	/* INITIALIZATION VECTOR MUST BE SAME SIZE AS BLOCK SIZE (256 BITS) */
+
+	fread(ciphertext, 1, ciphertext_len, ciphertext_file); /* READ CIPHERTEXT EXCEPT IV */
+	fread(key, 1, sizeof(key), key_file);
+
+	rewind(ciphertext_file);
+	fseek(ciphertext_file, ciphertext_len, SEEK_SET);
+
+	fread(iv, 1, sizeof(iv), ciphertext_file);
+
+	ciphertext[ciphertext_len]  = '\0'; /* ZERO OUT THE fread() TO THE END OF THE CIPHERTEXT */
+
+	Tawny_CTX ctx;
+	Tawny_Init(&ctx);
+
+	/* int Tawny_Update(int mode, Tawny_CTX * ctx, unsigned char * iv, unsigned char * key, unsigned char * plaintext, unsigned char * ciphertext, size_t plaintext_len, size_t ciphertext_len); */
+
+	if (!Tawny_Update(TAWNY_UPDATE_CIPHERTEXT, &ctx, NULL, NULL, NULL, ciphertext, 0, ciphertext_len)) {
+		fprintf(stderr, "Tawny_Update() failed to update the ciphertext\nAborting...\n");
+		return -1;
+	}
+
+	if (!Tawny_Update(TAWNY_UPDATE_KEY, &ctx, NULL, key, NULL, NULL, 0, 0)) {
+		fprintf(stderr, "Tawny_Update() failed to update the encryption key\nAborting...\n");
+		return -1;
+	}
+
+	if (!Tawny_Update(TAWNY_UPDATE_IV, &ctx, iv, NULL, NULL, NULL, 0, 0)) {
+		fprintf(stderr, "Tawny_Update() failed to update the IV\nAborting...\n");
+		return -1;
+	}
+
+	size_t bytes_written = 0;
+
+	/* Tawny_Decrypt RETURNS THE AMOUNT OF COPIED BYTES ON SUCCESS AND 0 ON FAILURE */
+
+	bytes_written = Tawny_Decrypt(&ctx);
+
+	if (bytes_written < 1) {
+		fprintf(stderr, "Tawny_Decrypt() failed.\nAborting...\n");
+		return -1;
+	}
+
+	/* PLAINTEXT IS NOW STORED IN ctx.plaintext */
+	/* LENGTH OF PLAINTEXT IS STORED IN ctx.plaintext_len */
+
+	char outfile_name[strlen(argv[1]) + 10];
+
+	snprintf(outfile_name, sizeof(outfile_name), "%s", "decrypted.txt");
+
+	FILE * outfile = fopen(outfile_name, "wb");
+
+	if (outfile == NULL) {
+		fprintf(stderr, "Failed to open file file for writing \"%s\" [fopen()]\n", (ciphertext_file == NULL)?argv[1]:argv[2]);
+		perror("Error");
+		return -1;
+	}
+
+	/* WRITE PLAINTEXT TO NEW FILE */
+
+	fwrite(ctx.plaintext, ctx.plaintext_len, 1, outfile);
+
+	printf("Success!\nDecrypted data now stored in \"%s\"\n", outfile_name);
+	printf("Data: %s\n", ctx.plaintext);
+
+	return 0;
+}
---
+++ examples/encrypt.c
@@ -0,0 +1,112 @@
+#include <stdio.h>
+#include <string.h>
+#include <openssl/rand.h>
+
+#include "tawny.h"
+
+int main(int argc, char ** argv) {
+	if (argc < 3) {
+		fprintf(stderr, "\nUsage: [PLAINTEXT FILE] [KEY FILE]\n\n");
+		return -1;
+	}
+
+	char * plaintext_filename = argv[1];
+	char * key_filename = argv[2];
+
+	FILE * plaintext_file = fopen(plaintext_filename, "r");
+	FILE * key_file = fopen(key_filename, "rb");
+
+	if (plaintext_file == NULL || key_file == NULL) {
+		fprintf(stderr, "Failed to open file \"%s\" [fopen()]\n", (plaintext_file == NULL)?argv[1]:argv[2]);
+		perror("Error");
+		return -1;
+	}
+
+	long plaintext_len, key_len = 0;
+
+	fseek(plaintext_file, 0, SEEK_END); 	/* GO TO END OF PLAINTEXT FILE */
+	fseek(key_file, 0, SEEK_END); 		/* GET FIRST 32 BYTES OF KEY */
+
+	plaintext_len = ftell(plaintext_file);	/* STORE LENGTH OF PLAINTEXT FILE */
+	key_len = ftell(key_file);		/* STORE LENGTH OF ENCRYPTION KEY - 2 FOR LINE ENDING */
+
+	rewind(plaintext_file);	/* RESET POSITION OF FILES TO THE START */
+	rewind(key_file);
+
+	if (key_len != TAWNY_BLOCK_SIZE_BYTES) { 	/* CHECK LENGTH OF KEY */
+		fprintf(stderr, "Key length incorrect (%ld)\nKey size must be 256 bits (32 bytes)\n", key_len);
+		return -1;
+	}
+
+	char plaintext[plaintext_len];
+	char key[TAWNY_BLOCK_SIZE_BYTES];
+	char iv[TAWNY_BLOCK_SIZE_BYTES];	/* INITIALIZATION VECTOR MUST BE SAME SIZE AS BLOCK SIZE (256 BITS) */
+
+	if (!RAND_bytes(iv, sizeof(iv))) {
+		fprintf(stderr, "Failed to randomly generate IV [RAND_bytes()] (NOT CRITICAL BUT UNSAFE)\nAborting...");
+		return -1;
+	}
+
+	fread(plaintext, 1, plaintext_len, plaintext_file);
+	fread(key, 1, sizeof(key), key_file);
+
+	plaintext[plaintext_len] = '\0';	/* ZERO OUT THE fread() */
+
+	/* PAD THE PLAINTEXT TO THE REQUIRED BLOCK SIZE DIVISION */
+	char * padded_plaintext = pkcs7pad(plaintext, strlen(plaintext), TAWNY_BLOCK_SIZE_BYTES);
+
+	Tawny_CTX ctx;
+	Tawny_Init(&ctx);
+
+	/* int Tawny_Update(int mode, Tawny_CTX * ctx, unsigned char * iv, unsigned char * key, unsigned char * plaintext, unsigned char * ciphertext, size_t plaintext_len, size_t ciphertext_len); */
+
+	if (!Tawny_Update(TAWNY_UPDATE_PLAINTEXT, &ctx, NULL, NULL, padded_plaintext, NULL, strlen(padded_plaintext), 0)) {
+		fprintf(stderr, "Tawny_Update() failed to update the plaintext\nAborting...\n");
+		return -1;
+	}
+
+	if (!Tawny_Update(TAWNY_UPDATE_KEY, &ctx, NULL, key, NULL, NULL, 0, 0)) {
+		fprintf(stderr, "Tawny_Update() failed to update the encryption key\nAborting...\n");
+		return -1;
+	}
+
+	if (!Tawny_Update(TAWNY_UPDATE_IV, &ctx, iv, NULL, NULL, NULL, 0, 0)) {
+		fprintf(stderr, "Tawny_Update() failed to update the IV\nAborting...\n");
+		return -1;
+	}
+
+	size_t bytes_written = 0;
+
+	/* Tawny_Encrypted RETURNS THE AMOUNT OF COPIED BYTES ON SUCCESS AND 0 ON FAILURE */
+
+	bytes_written = Tawny_Encrypt(&ctx);
+
+	if (bytes_written < 1) {
+		fprintf(stderr, "Tawny_Encrypt() failed.\nAborting...\n");
+		return -1;
+	}
+
+	/* ENCRYPTED TEXT/CIPHERTEXT IS NOW STORED IN ctx.ciphertext */
+	/* LENGTH OF CIPHERTEXT IS STORED IN ctx.ciphertext_len */
+
+	char outfile_name[strlen(argv[1]) + 10];
+
+	snprintf(outfile_name, sizeof(outfile_name), "%s.tawnyenc", plaintext_filename);
+
+	FILE * outfile = fopen(outfile_name, "wb");
+
+	if (outfile == NULL) {
+		fprintf(stderr, "Failed to open file file for writing \"%s\" [fopen()]\n", (plaintext_file == NULL)?argv[1]:argv[2]);
+		perror("Error");
+		return -1;
+	}
+
+	/* WRITE CIPHERTEXT TO NEW FILE */
+
+	fwrite(ctx.ciphertext, ctx.ciphertext_len, 1, outfile);
+	fwrite(iv, TAWNY_BLOCK_SIZE_BYTES, 1, outfile);
+
+	printf("Success!\nEncrypted data now stored in \"%s\"\n", outfile_name);
+
+	return 0;
+}
